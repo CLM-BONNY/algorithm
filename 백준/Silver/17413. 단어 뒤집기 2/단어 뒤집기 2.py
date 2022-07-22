7ef33c323d408bca626cbf94adb33c3e4978f3a9
MinSeon Kim <alstjs4739@kookmin.ac.kr>
2022-07-22 12:03:54 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 304 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,25 @@
+S = input()
+flag = False
+word = ""
+answer = ""
+
+for i in S:
+    if flag == False:
+        if i == "<":
+            flag = True
+            word += i
+        elif i == " ":
+            word += i
+            answer += word
+            word = ""
+        else:
+            word = i + word
+
+    else:
+        word += i
+        if i == ">":
+            flag = False
+            answer += word
+            word = ""
+
+print(answer + word)
