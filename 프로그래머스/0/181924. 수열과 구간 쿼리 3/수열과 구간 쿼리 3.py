38c0baa528d9197c27a24e8e3f136cfd482c3ed6
MinSeon Kim <alstjs4739@kookmin.ac.kr>
2024-02-15 11:38:08 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.14 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -0,0 +1,8 @@
+def solution(arr, queries):
+    for x in range(len(queries)):
+        i = queries[x][0]
+        j = queries[x][1]
+        
+        arr[i], arr[j] = arr[j], arr[i]
+    
+    return arr
