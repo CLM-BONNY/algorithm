efff8da9d320583d356b9c4b5b9027a6768469b0
MinSeon Kim <alstjs4739@kookmin.ac.kr>
2024-02-15 11:48:22 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 18.07 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = arr
+    
+    for i in range(len(arr)):
+        if answer[i] >= 50 and answer[i] % 2 == 0:
+            answer[i] /= 2
+        elif answer[i] < 50 and answer[i] % 2 == 1:
+            answer[i] *= 2
+    
+    return answer
