4a41afa3eef182bbfcc050a420eed04f9f8ec75b
MinSeon Kim <alstjs4739@kookmin.ac.kr>
2025-08-22 18:10:08 +0900

    [level 2] Title: H-Index, Time: 0.25 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,12 @@
 def solution(citations):
+    answer = 0
+    
     citations.sort(reverse=True)
     
-    for i, c in enumerate(citations):
-        if i + 1 > c:
-            return i
-            
-    return len(citations)
+    for i, v in enumerate(citations):
+        if v >= i+1:
+            answer = i+1
+        else:
+            break
+
+    return answer
